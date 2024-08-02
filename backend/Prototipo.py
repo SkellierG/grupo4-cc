010223e3f0fa706d7398f6667f96ac79e1b671b7
ELDSC <sarangojosue6@gmail.com>
2024-08-02 17:11:24 -0500

    Avance codigo

--- backend/Prototipo.py
+++ backend/Prototipo.py
@@ -8,18 +8,39 @@ class JuegoAdivinarNumero:
         self.intentos = 0
         
     def Bienvenida(self):
-        print("""Que tal usuario :D !!!         
-        Bienvenido al juego de Adivanzas de Numeros
-        OJO: El numero random ha adivinar va desde 1 al 50
-        """) 
+        return {
+            "message": """Que tal usuario :D !!!         
+            Bienvenido al juego de Adivanzas de Numeros
+            OJO: El numero random ha adivinar va desde 1 al 50
+            """}
         
     def SolicitarNumero(self):
-        while (self.intentos <= self.intentosMaximos):
-            print("Numero de intento: ", (self.intentos+1))
+        while (self.intentos < self.intentosMaximos):
+            print(f"Numero de intento: ", (self.intentos+1))
             try:
                 respuesta = int(input("Ingrese Numero: "))
                 if (respuesta < self.valorminimo or respuesta > self.valormaximo):
                     print("ERROR! Ingresa numero valido")
-                    break
             except ValueError:
                 print("ERROR! Ingresa numero valido")
+                
+    def Resultado(self):
+        return {"message": f"Has agotado tus {self.intentosMaximos} intentos :( ----> El numero era: {self.numeroRandom}"}
+    
+    def juego(self):
+        self.Bienvenida()
+        while (self.intentos <= self.intentosMaximos):
+            respuesta = self.SolicitarNumero()
+            self.intentos += 1
+            if respuesta == self.numeroRandom:
+                return{"message": """Felicidades! Adivinaste el Numero! Solo te tomo {self.intentos} intentos xd
+                Fin del juego..."""}
+            elif respuesta > self.numeroRandom:
+               return{"message": "El numero ingresado es mayor al numero random"}
+            elif respuesta < self.numeroRandom:
+                return{"message": "El numero ingresado es menor al numero random"}
+            
+        if self.intentos >= self.intentosMaximos and respuesta != self.numeroRandom:
+            return self.Resultado
+juegoIniciar =  JuegoAdivinarNumero()
+juegoIniciar.juego()
