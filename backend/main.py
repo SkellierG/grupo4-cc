4969c00d70c546836d7ccdbc4b27486e6c99872e
SkellierG <benja314gonzalez@gmail.com>
2024-08-02 16:49:45 -0400

    README and pydantic in rest

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, ValidationError
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return { message: "root" }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
 
