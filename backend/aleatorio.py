48113019da0d7cd9a1dc55eb9e9963a0e8da7039
ELDSC <sarangojosue6@gmail.com>
2024-08-02 13:36:56 -0500

    Modifique el codigo, y adjunte dos validaciones

--- backend/aleatorio.py
+++ backend/aleatorio.py
@@ -2,7 +2,7 @@ import random
 intentos = 0
 NumeroAleatorio = random.randint(1,50) #---------Se utiliza el .randint para que aparezcan solo enteros, y lo que esta entre parentesis es para crear parametros
 print(NumeroAleatorio) #------------Hacer trampa sabiendo el numero random, para verificar
-print("""Que tal usuario 😄 !!!         
+print("""Que tal usuario :D !!!         
 Bienvenido al juego de Adivanzas de Numeros
 OJO: El numero random ha adivinar va desde 1 al 50
 """)        #-----------------Bienvenida
@@ -10,11 +10,17 @@ while (intentos <= 3):
     print("Numero de intento: ", intentos)
     print("Ingrese Numero:") #-------------Solicitud de numero
     Respuesta = int(input())
+    if (Respuesta <=0):
+        print("ERROR! Numero no valido")
+        break #---------------------Use el break para terminar el juego / Validacion
+    elif(Respuesta >=50):
+        print("ERROR! Numero no valido") 
+        break #---------------------Use el break para terminar el juego / Validacion
     if(Respuesta == NumeroAleatorio):
         print("Felicidades! Adivinaste el Numero!")
         print("Solo te tomo ", intentos, "intentos xd")
         print("Fin del juego...")
-        break
+        break 
     elif(Respuesta > NumeroAleatorio):
         print("El numero ingresado es mayor al numero random")
         intentos += 1
@@ -23,5 +29,5 @@ while (intentos <= 3):
         intentos += 1
 if intentos > 3: #--------------Aqui ya acaba el juego, revelando el numero aleatorio
     print("============RESULTADOS============")
-    print("Has agotado tus 3 intentos 😦 ----> El numero era:", NumeroAleatorio)
+    print("Has agotado tus 3 intentos :( ----> El numero era:", NumeroAleatorio)
     print("==================================")
