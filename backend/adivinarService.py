51765b8bcfe318ee8fa4062c2552ddaa9b840658
ELDSC <sarangojosue6@gmail.com>
2024-08-04 12:00:08 -0500

    Aqui esta el avance

--- backend/adivinarService.py
+++ backend/adivinarService.py
@@ -5,3 +5,35 @@
 from adivinarInterface import Iadivinar
 
 class Sadivinar(Iadivinar):
+    def __init__(self, valorminimo=1, valormaximo=50, intentosMaximos=3):
+        super().__init__()
+        self.valorminimo = valorminimo
+        self.valormaximo = valormaximo
+        self.intentosMaximos = intentosMaximos
+        self.intentos = 0
+        self.hidden_number = None
+    
+    def getNumber(self):
+        return {'hidden_number': self.hidden_number}
+    
+    def setNumber(self):
+        self.hidden_number = random.randint(self.valorminimo, self.valormaximo)
+        self.intentos = self.intentosMaximos
+        return {'created': True}
+    
+    def compareNumbers(self, number):
+        self.intentos -= 1
+        feedback = self.feedback(number)
+        return {'feedback': feedback}
+    
+    def feedback(self, number):
+        if number < self.hidden_number:
+            return "El número es mayor."
+        elif number > self.hidden_number:
+            return "El número es menor."
+        else:
+            return f"¡Has acertado! El número escondido era {self.hidden_number}."
+    
+    def retry(self):
+        self.hidden_number = None
+        self.intentos = 0
